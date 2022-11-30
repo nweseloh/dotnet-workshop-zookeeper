ab9d30c48694708606d76ca577ffb3c27fdba127
Nicole Weseloh <nweseloh@neusta.de>
2022-11-30 11:20:08 +0100

    Initial commit

---
+++ Zookeeper/Controllers/WelcomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zookeeper.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WelcomeController : ControllerBase
+{
+    [HttpGet(Name = "GetWelcome")]
+    public string Get()
+    {
+        return "Welcome to the Zoo!";
+    }
+}
