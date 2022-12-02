eada30ab3398287a8f68979d798cc3b65a2be9f2
Nicole Weseloh <nweseloh@neusta.de>
2022-12-02 17:16:13 +0100

    MailService

---
+++ Zookeeper/Tools/EmailService.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Mail;
+
+namespace Zookeeper.Tools;
+
+public class EmailService
+{
+    public static void SendMail(string recipient)
+    {
+        SmtpClient smtpClient = new SmtpClient("127.0.0.1", 125);
+        smtpClient.Send("pr@zoo.com", recipient, "Wanna see something cute?", "Then come to our zoo!" );
+    }
+
+    public static bool IsValidEmailAddress(string emailAddress)
+    {
+        return emailAddress.Contains("@");
+    }
+}
